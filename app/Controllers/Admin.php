7fb7411659283762b008dcdd5f6ab10eac317b2d
Andriyan <andriyann77@gmail.com>
2023-07-26 21:36:42 +0800

    fix2

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,12 +2,48 @@
 
 namespace App\Controllers;
 
+use App\Models\Mod_penduduk;
+use App\Models\Mod_permintaan;
+use App\Models\Mod_datang;
+use App\Models\Mod_ahliwaris;
+use App\Models\Mod_izinusaha;
+use App\Models\Mod_kematian;
+use App\Models\Mod_kelahiran;
+use App\Models\Mod_skk;
+use App\Models\Mod_skktp;
+use App\Models\Mod_pindah;
+use App\Models\Mod_tidak_mampu;
+
+
+
 class Admin extends BaseController
 {
     protected $session;
+    protected $Mod_pindah;
+    protected $Mod_penduduk;
+    protected $Mod_ahliwaris;
+    protected $Mod_tidak_mampu;
+    protected $Mod_skk;
+    protected $Mod_skktp;
+    protected $Mod_kematian;
+    protected $Mod_kelahiran;
+    protected $Mod_izinusaha;
+    protected $Mod_permintaan;
+    protected $Mod_datang;
     public function __construct()
     {
         $this->session = session();
+        $this->Mod_penduduk = new Mod_penduduk();
+        $this->Mod_ahliwaris = new Mod_ahliwaris();
+        $this->Mod_tidak_mampu = new Mod_tidak_mampu();
+        $this->Mod_kematian = new Mod_kematian();
+        $this->Mod_skk = new Mod_skk();
+        $this->Mod_pindah = new Mod_pindah();
+        $this->Mod_skktp = new Mod_skktp();
+        $this->Mod_kelahiran = new Mod_kelahiran();
+        $this->Mod_izinusaha = new Mod_izinusaha();
+        $this->Mod_datang = new Mod_datang();
+        $this->Mod_permintaan = new Mod_permintaan();
     }
 
     public function index()
@@ -22,6 +58,32 @@ class Admin extends BaseController
             return redirect()->to('User/index');
         }
 
-        return view('user/index');
+        $penduduk = $this->Mod_penduduk->countAllResults();
+        $permintaan = $this->Mod_permintaan->countAllResults();
+        $ahliwaris = $this->Mod_ahliwaris->countAllResults();
+        $datang = $this->Mod_datang->countAllResults();
+        $usaha = $this->Mod_izinusaha->countAllResults();
+        $lahir = $this->Mod_kelahiran->countAllResults();
+        $mati = $this->Mod_kematian->countAllResults();
+        $kk = $this->Mod_skk->countAllResults();
+        $pindah = $this->Mod_pindah->countAllResults();
+        $ktp = $this->Mod_skktp->countAllResults();
+        $miskin = $this->Mod_tidak_mampu->countAllResults();
+
+        $data = [
+            'penduduk' => $penduduk,
+            'permintaan' => $permintaan,
+            'ahliwaris' => $ahliwaris,
+            'datang' => $datang,
+            'miskin' => $miskin,
+            'usaha' => $usaha,
+            'pindah' => $pindah,
+            'lahir' => $lahir,
+            'mati' => $mati,
+            'kk' => $kk,
+            'ktp' => $ktp,
+        ];
+
+        return view('user/index', $data);
     }
 }
--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -41,6 +41,122 @@
             </div>
             <!-- // -------------------------------------------------------------------------------------------------------------- -->
             <hr>
+            <div class="row">
+
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Penduduk</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $penduduk; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data permintaan Penduduk</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $permintaan; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data ahliwaris</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $ahliwaris; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Datang Penduduk</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $datang; ?></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Keterangan Miskin</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $miskin; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Izin Usaha</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $usaha; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Pindah</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $pindah; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Kelahiran</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $lahir; ?></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data Kematian</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $mati; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data KK</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $kk; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header">Data KTP</div>
+                        <div class="card-body">
+                            <h6>Jumlah Data</h6>
+                            <h3 class="card-title"><?= $ktp; ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col"></div>
+
+            </div>
+
+            <div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15946.185981523808!2d115.44704618715816!3d-2.3205225000000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2de54f6219795919%3A0x915f6d82f0c14a7e!2sKantor%20CAMAT%20Paringin!5e0!3m2!1sen!2sid!4v1690378529984!5m2!1sen!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
     </main>
     <?= $this->include('nav/foot'); ?>
